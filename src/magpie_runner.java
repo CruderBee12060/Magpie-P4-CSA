a7165aade356690c4a50eb2d8a361ad18182d133
nathan <nathanlee0209@yahoo.com>
2020-02-20 19:04:47 -0800

    Initial commit

---
+++ src/magpie_runner.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+
+public class magpie_runner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello world");
+    }
+}
